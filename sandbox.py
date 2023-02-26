45bb719e057e812aa8922948812741fa7e8a3b30
cseibert2667 <cseibert2667@gmail.com>
2023-02-25 22:11:58 -0700

    more function fun lol

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,10 @@
 # LISTS (arrays) & TUPLES (immutable)
 
 friends = ["Dane", "Jamie", "Semp", "Megan", "Bonnie"]
-numbers = [69, 420, 1337, (3, 15)] # 3 & 15 are TUPLES
+numbers = [69, 420, 1337, (3), (15), 20, 360, 112, 99] # 3 & 15 are TUPLES
 
 
-print(friends[1:4]) # prints indexs 1 - 3
+# print(friends[1:4]) # prints indexs 1 - 3
 
 friends.append("Joey") # added Joey to friends at end of list
 
@@ -12,16 +12,20 @@ friends.insert(2, "Lisa") # inserts Lisa to friends at index 2
 
 friends.remove("Bonnie") # removes Bonnie
 
-print(friends.index("Lisa")) # prints index of Lisa
+# print(friends.index("Lisa")) # prints index of Lisa
 
 friends.sort() # alphabetical // ascending order
 
-print(numbers) 
-
 # FUNCTIONS, LOOPS, IFS
 
-def say_hello():
+def say_hello(): # says hello to each item in friends list
     for friend in friends:
         print("Hello " + friend)
 
-say_hello()
+def even_or_odd(): # determines if each number is even or odd and prints it
+    for number in numbers:
+        if number % 2 == 0:
+            print(str(number) + " is even")
+        else:
+            print(str(number) + " is odd")
+
