9f2c99241717a9adab4e8bc66ed542c91534c960
cseibert2667 <cseibert2667@gmail.com>
2023-02-25 22:28:38 -0700

    anotha one

--- sandbox.py
+++ sandbox.py
@@ -2,6 +2,7 @@
 
 friends = ["Dane", "Jamie", "Semp", "Megan", "Bonnie"]
 numbers = [69, 420, 1337, (3), (15), 20, 360, 112, 99] # 3 & 15 are TUPLES
+other_numbers = [30, 15, 24]
 
 
 # print(friends[1:4]) # prints indexs 1 - 3
@@ -18,14 +19,26 @@ friends.sort() # alphabetical // ascending order
 
 # FUNCTIONS, LOOPS, IFS
 
-def say_hello(): # says hello to each item in friends list
+# says hello to each item in friends list
+def say_hello(): 
     for friend in friends:
         print("Hello " + friend)
 
-def even_or_odd(): # determines if each number is even or odd and prints it
+# determines if each number is even or odd and prints it
+def even_or_odd(): 
     for number in numbers:
         if number % 2 == 0:
             print(str(number) + " is even")
         else:
             print(str(number) + " is odd")
 
+# finds average of any numerical list, rounded to any specified number of digits
+def find_average(nums, x):
+    sum = 0
+    count = 0
+    for num in nums:
+        sum = sum + num
+        count += 1
+    print(round(sum / count, x))
+
+find_average(numbers, 3)
