35d58e59d23ea9e309e8c2f5b2fc85bf4c3b8c9a
cseibert2667 <cseibert2667@gmail.com>
2023-02-25 20:02:46 -0700

    paint needed calculation + other variable type casting

--- project1.py
+++ project1.py
@@ -1,7 +1,9 @@
-wall_height = input("Enter height:")
-wall_width = input("Enter width:")
-paint_cost = input("Enter cost of paint:")
-wall_area = int(wall_height) * int(wall_width)
+wall_height = float(input("Enter height:"))
+wall_width = float(input("Enter width:"))
+# paint_cost = input("Enter cost of paint:")
+wall_area = round(wall_height * wall_width, 2)
+paint_needed = round(wall_area / 350.0, 3)
 
 
-print(wall_area)
+print("Wall area: " + str(wall_area))
+print("Paint needed: " + str(paint_needed) + " gallons.")
