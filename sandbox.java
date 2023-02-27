714b02398ea920a61315b314d90657974500320f
Cooper <cseibert2667@gmail.com>
2023-02-27 10:39:28 -0700

    java sandbox w/ 4.14 lab copied in

---
+++ sandbox.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class sandbox {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int n;
+      int i;
+   
+      System.out.println("Enter an integer:");
+      n = scnr.nextInt();
+   
+      System.out.print("Sequence: ");
+      if (n % 2 != 0) {
+         n--;
+      }
+      if (n < 0) {
+         System.out.println("0 ");
+      } else {
+      for (i = n; i >= 0; i -= 2) {
+         System.out.print(i + " ");
+      }
+      System.out.println();
+      }
+   }
+}
