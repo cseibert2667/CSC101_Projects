0df894399fac94eda916e97bfbf95646dcadd1dd
Cooper <cseibert2667@gmail.com>
2023-03-01 15:40:29 -0700

    lab 4.20 done

--- sandbox.java
+++ sandbox.java
@@ -4,15 +4,25 @@ public class sandbox {
    public static void main(String[] args) {
       try (Scanner scnr = new Scanner(System.in)) {
 
+         // get input
          String userString = scnr.nextLine();
-         String newString = "";
+         String backwards = "";
 
-         for (int i = 0; i < userString.length(); i++) {
-            char currentChar = userString.charAt(i);
-            if (Character.isLetter(currentChar)) {
-               newString += userString.substring(i, i+1);
+         // if input != "done", "Done", or "d"
+         while (!userString.equals("done") && !userString.equals("Done") && !userString.equals("d")){
+            // iterate through each character, starting from the last
+            for (int i = userString.length() - 1; i >= 0; i--){
+               // add each character to a new string until out of characters
+               backwards += userString.substring(i, i+1);
             }
-            System.out.println(newString); //print every iteration
+            // print
+            System.out.println(backwards);
+            // get new input
+            userString = scnr.nextLine();
+            // clear out backwards string
+            backwards = "";
+            // start back at while loop
+            continue;
          }
 
       }
