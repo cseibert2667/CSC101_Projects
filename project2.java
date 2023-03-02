121e0444fc76c11569f7c64f98a58aa9e6197ee9
cseibert2667 <cseibert2667@gmail.com>
2023-03-01 17:50:14 -0700

    variables for user inputs

--- project2.java
+++ project2.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class project2 {
-    public static void main(String []args) {
-    Scanner scnr = new Scanner(System.in);
-    final int ROCK = 0;
-    final int PAPER = 1;
-    final int SCISSORS = 2;
-    Random rand = new Random();
-    int seed = scnr.nextInt();
-    rand.setSeed(seed);
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        final int ROCK = 0;
+        final int PAPER = 1;
+        final int SCISSORS = 2;
+        Random rand = new Random();
+        int seed = scnr.nextInt();
+        rand.setSeed(seed);
+        // get inputs
+        String player1 = scnr.next();
+        String player2 = scnr.next();
+        int numRounds = scnr.nextInt();
 
-    
-
+        System.out.println(player1);
+        System.out.println(player2);
+        System.out.println(numRounds);
     }
 }
