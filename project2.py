82660b523ee56e8012fa5e2e721bcd24f03025c2
Cooper <cseibert2667@gmail.com>
2023-03-02 10:13:58 -0700

    initial inputs & checks

--- project2.py
+++ project2.py
@@ -1,9 +1,19 @@
 import random
 
 # 1 = rock, 2 = paper, 3 = scissors
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
 # get inputs for player names & num_rounds
+player_1 = input("Enter player 1 name: ")
+player_2 = input("Enter player 2 name: ")
+num_rounds = int(input("Enter number of rounds: "))
 # make sure num_rounds is positive
+while (num_rounds < 0):
+    print("Rounds must be > 0")
+    num_rounds = int(input("Enter number of rounds: "))
 # print p1 vs p2 for x rounds
+print(player_1,"vs",player_2,"for",num_rounds,"rounds")
 # random player inputs (random.randint(1,3))
 # loop for number of rounds
     # print tie if inputs match
