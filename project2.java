bba14ea633c7e69a3bafd95e27ab63c997711802
cseibert2667 <cseibert2667@gmail.com>
2023-03-01 20:29:29 -0700

    cleanup & notation

--- project2.java
+++ project2.java
@@ -10,10 +10,12 @@ public class project2 {
         Random rand = new Random();
         int seed = scnr.nextInt();
         rand.setSeed(seed);
+
         // get inputs
         String player1 = scnr.next();
         String player2 = scnr.next();
         int numRounds = scnr.nextInt();
+
         // other variables
         int player1Input;
         int player2Input;
@@ -28,11 +30,13 @@ public class project2 {
         // print player1 vs player2 for x rounds
         System.out.println(player1 + " vs " + player2 + " for " + numRounds + " rounds");
 
-        // random inputs
+        // random player inputs
         player1Input = rand.nextInt(3);
         player2Input = rand.nextInt(3);
-        
+
+        // loop through for number of rounds
         for (int i = 1; i <= numRounds; i++) {
+
         // print tie if values the same
         while (player1Input == player2Input) {
             System.out.println("Tie");
@@ -40,47 +44,42 @@ public class project2 {
             player2Input = rand.nextInt(3);
         }
 
-            // identify winner
+            // identify winner, increment player wins accordingly, get new random player inputs
             if (player2Input == SCISSORS && player1Input == PAPER) {
                 System.out.println(player2 + " wins with scissors");
                 player2wins++;
                 player1Input = rand.nextInt(3);
                 player2Input = rand.nextInt(3);
-                //continue;
             } else if (player2Input == ROCK && player1Input == SCISSORS) {
                 System.out.println(player2 + " wins with rock");
                 player2wins++;
                 player1Input = rand.nextInt(3);
                 player2Input = rand.nextInt(3);
-                //continue;
             } else if (player2Input == PAPER && player1Input == ROCK) {
                 System.out.println(player2 + " wins with paper");
                 player2wins++;
                 player1Input = rand.nextInt(3);
                 player2Input = rand.nextInt(3);
-                //continue;
             } else if (player1Input == SCISSORS && player2Input == PAPER) {
                 System.out.println(player1 + " wins with scissors");
                 player1wins++;
                 player1Input = rand.nextInt(3);
                 player2Input = rand.nextInt(3);
-                //continue;
             } else if (player1Input == ROCK && player2Input == SCISSORS) {
                 System.out.println(player1 + " wins with rock");
                 player1wins++;
                 player1Input = rand.nextInt(3);
                 player2Input = rand.nextInt(3);
-                //continue;
             } else if (player1Input == PAPER && player2Input == ROCK) {
                 System.out.println(player1 + " wins with paper");
                 player1wins++;
                 player1Input = rand.nextInt(3);
                 player2Input = rand.nextInt(3);
-                //continue;
             }
 
         }
 
+        // print final score
         System.out.println(player1 + " wins " + player1wins + " and " + player2 + " wins " + player2wins);
         
         
