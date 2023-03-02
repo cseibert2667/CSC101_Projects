3f851fbd5205e9914dab30dbfd2d5d8060a6f825
cseibert2667 <cseibert2667@gmail.com>
2023-03-01 17:32:44 -0700

    copied starting code for project 2

---
+++ project2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class project2 {
+    public static void main(String []args) {
+    Scanner scnr = new Scanner(System.in);
+    final int ROCK = 0;
+    final int PAPER = 1;
+    final int SCISSORS = 2;
+    Random rand = new Random();
+    int seed = scnr.nextInt();
+    rand.setSeed(seed);
+
+    
+
+    }
+}
