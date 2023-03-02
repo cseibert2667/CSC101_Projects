3123d55c36aa3a43a900e24ff00c58865be4887f
cseibert2667 <cseibert2667@gmail.com>
2023-03-01 21:56:20 -0700

    outline for project 2

---
+++ project2.py
@@ -0,0 +1,11 @@
+import random
+
+# 1 = rock, 2 = paper, 3 = scissors
+# get inputs for player names & num_rounds
+# make sure num_rounds is positive
+# print p1 vs p2 for x rounds
+# random player inputs (random.randint(1,3))
+# loop for number of rounds
+    # print tie if inputs match
+    # identify winner, increment player wins, get new random inputs
+# print final score
