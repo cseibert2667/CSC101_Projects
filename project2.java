a1d88d66a63bab4adb5e8d7b472000755f6cff8c
cseibert2667 <cseibert2667@gmail.com>
2023-03-01 18:01:11 -0700

    check for numRounds > 0

--- project2.java
+++ project2.java
@@ -15,8 +15,22 @@ public class project2 {
         String player2 = scnr.next();
         int numRounds = scnr.nextInt();
 
-        System.out.println(player1);
-        System.out.println(player2);
-        System.out.println(numRounds);
+        // numRounds must be a positive number
+        while (numRounds < 0){
+            System.out.println("Rounds must be > 0");
+            numRounds = scnr.nextInt();
+            continue;
+        }
+        // print player1 vs player2 for x rounds
+        System.out.println(player1 + " vs " + player2 + " for " + numRounds + " rounds");
+
+        
+
+        
+        // TEST PRINTS
+        // System.out.println(player1);
+        // System.out.println(player2);
+        // System.out.println(numRounds);
+        
     }
 }
