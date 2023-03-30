928615f23054749b352e14f8d3bba090c097b7b5
cseibert2667 <cseibert2667@gmail.com>
2023-03-29 19:03:12 -0700

    output above rating func

--- project3.java
+++ project3.java
@@ -43,13 +43,25 @@ public class project3 {
                             rating[i] = scnr.nextInt();
                         }
                     }
+                    System.out.println();
                     continue;
                 }
                 if (menuOption == 'a') { // output above rating
-
+                    System.out.println("Enter a rating:");
+                    int thresh = scnr.nextInt();
+                    System.out.println("ABOVE " + thresh);
+                    for (int i = 0; i < rating.length; i++) {
+                        if (rating[i] > thresh) {
+                            System.out
+                                    .println("Player " + (i + 1) + " -- Jersey number: " + jerseyNum[i] + ", Rating: "
+                                            + rating[i]);
+                        }
+                    }
+                    System.out.println();
+                    continue;
                 }
                 if (menuOption == 'r') { // replace player
-
+                    
                 }
                 if (menuOption == 'o') { // output roster
                     System.out.println("ROSTER");
