93c116b8821def94b75ffec70b8d026275df660a
cseibert2667 <cseibert2667@gmail.com>
2023-03-29 18:42:49 -0700

    update & output roster functions

--- project3.java
+++ project3.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 
 public class project3 {
     public static void main(String[] args) {
-        try (Scanner scnr = new Scanner(System.in)){
+        try (Scanner scnr = new Scanner(System.in)) {
             int[] jerseyNum = new int[5];
             int[] rating = new int[5];
             char menuOption;
 
             // get inputs
             for (int i = 0; i < jerseyNum.length; i++) {
-                System.out.println("Enter Player " + (i+1) + "'s jersey number:");
+                System.out.println("Enter Player " + (i + 1) + "'s jersey number:");
                 jerseyNum[i] = scnr.nextInt();
-                System.out.println("Enter Player " + (i+1) + "'s rating:");
+                System.out.println("Enter Player " + (i + 1) + "'s rating:");
                 rating[i] = scnr.nextInt();
             }
 
             // print roster
             System.out.println("ROSTER");
             for (int i = 0; i < rating.length; i++) {
-                System.out.println("Player " + (i+1) + " -- Jersey number: " + jerseyNum[i] + ", Rating: " + rating[i]);
+                System.out
+                        .println("Player " + (i + 1) + " -- Jersey number: " + jerseyNum[i] + ", Rating: " + rating[i]);
             }
-
+            System.out.println();
             // menu
             do {
                 System.out.println("MENU");
@@ -31,10 +32,35 @@ public class project3 {
                 System.out.println("o - Output roster");
                 System.out.println("q - Quit");
                 System.out.println();
-                System.out.println("Choose an option: ");
+                System.out.println("Choose an option:");
                 menuOption = scnr.next().charAt(0);
-            }
-            while (menuOption != 'q');
+                if (menuOption == 'u') { // update player rating
+                    System.out.println("Enter a jersey number:");
+                    int update = scnr.nextInt();
+                    for (int i = 0; i < jerseyNum.length; i++) {
+                        if (jerseyNum[i] == update) {
+                            System.out.println("Enter a new rating for player:");
+                            rating[i] = scnr.nextInt();
+                        }
+                    }
+                    continue;
+                }
+                if (menuOption == 'a') { // output above rating
+
+                }
+                if (menuOption == 'r') { // replace player
+
+                }
+                if (menuOption == 'o') { // output roster
+                    System.out.println("ROSTER");
+                    for (int i = 0; i < rating.length; i++) {
+                        System.out.println(
+                                "Player " + (i + 1) + " -- Jersey number: " + jerseyNum[i] + ", Rating: " + rating[i]);
+                    }
+                    System.out.println();
+                    continue;
+                }
+            } while (menuOption != 'q'); // quit
         }
     }
 }
