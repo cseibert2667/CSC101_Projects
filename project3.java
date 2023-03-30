a8dca6c044b6f57910de8a95e89d374f424df54a
cseibert2667 <cseibert2667@gmail.com>
2023-03-29 18:00:38 -0700

    getting inputs for 2 arrays

--- project3.java
+++ project3.java
@@ -7,9 +7,18 @@ public class project3 {
             int[] jerseyNum = new int[5];
             int[] rating = new int[5];
 
-            
+            // get inputs
+            for (int i = 0; i < jerseyNum.length; i++) {
+                System.out.println("Enter Player " + (i+1) + "'s jersey number:");
+                jerseyNum[i] = scnr.nextInt();
+                System.out.println("Enter Player " + (i+1) + "'s rating:");
+                rating[i] = scnr.nextInt();
+            }
 
-
+            // print roster
+            for (int i = 0; i < rating.length; i++) {
+                System.out.println("ROSTER");
+            }
         }
     }
 }
