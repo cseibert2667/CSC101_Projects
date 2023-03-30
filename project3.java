2c6e17f1474c4bd4ee99de891042e6997552e06b
cseibert2667 <cseibert2667@gmail.com>
2023-03-29 17:49:14 -0700

    project 3 start

---
+++ project3.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class project3 {
+    public static void main(String[] args) {
+        try (Scanner scnr = new Scanner(System.in)){
+            int[] jerseyNum = new int[5];
+            int[] rating = new int[5];
+
+            
+
+
+        }
+    }
+}
