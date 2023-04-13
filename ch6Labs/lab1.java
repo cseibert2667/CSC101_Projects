1d4ea9e8f9a6091cde4be74354d83a72d276e202
cseibert2667 <cseibert2667@gmail.com>
2023-04-12 17:53:34 -0700

    ch6 labs & lab 1 start

---
+++ ch6Labs/lab1.java
@@ -0,0 +1,16 @@
+package ch6Labs;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class lab1 {
+
+    public static void main (String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int array[] = new int[4];
+
+        for (int i = 0; i < array.length; i++) {
+            
+        }
+
+    }
+}
