5a69484899e2f7dbc15725437cd32958fdd9082d
Cooper <cseibert2667@gmail.com>
2023-04-13 10:22:10 -0700

    method defined

--- ch6Labs/lab4.java
+++ ch6Labs/lab4.java
@@ -5,12 +5,23 @@ import java.util.Arrays;
 
 public class lab4 {
     public static int getWordFrequency (String[] arr, int arrSize, String word) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equalsIgnoreCase(word)){
+                count++;
+            }
+        }
 
-
-        return 0;
+        return count;
     }
 
     public static void main (String[] args){
+        try (Scanner scnr = new Scanner(System.in)){
+            String[] wordList = new String [20];
+            int wordListLength = scnr.nextInt();
+
+            
+        };
 
     }
 }
