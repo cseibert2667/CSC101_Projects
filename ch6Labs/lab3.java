f12899adc192e02808747f79a5babdb63f5200d3
cseibert2667 <cseibert2667@gmail.com>
2023-04-12 18:33:20 -0700

    lab 3 start

---
+++ ch6Labs/lab3.java
@@ -0,0 +1,24 @@
+package ch6Labs;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class lab3 {
+
+    public static void sortArray(int[] myArr, int arrSize){
+
+    }
+
+
+    public static void main (String[] args){
+        Scanner scnr = new Scanner(System.in);
+        int[] numbers = new int[20];
+
+        int arrLength = scnr.nextInt();
+
+        for (int i = 0; i < arrLength; i++) {
+            numbers[i] = scnr.nextInt();
+        }
+
+    }
+}
