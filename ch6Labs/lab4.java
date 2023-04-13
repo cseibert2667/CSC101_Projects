4d33b53c3cc64ebe5d8f8eaa91dd1b4e42ba0438
Cooper <cseibert2667@gmail.com>
2023-04-13 10:37:44 -0700

    lab4 done

--- ch6Labs/lab4.java
+++ ch6Labs/lab4.java
@@ -1,13 +1,12 @@
 package ch6Labs;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class lab4 {
-    public static int getWordFrequency (String[] arr, int arrSize, String word) {
+    public static int getWordFrequency(String[] arr, int arrSize, String word) {
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equalsIgnoreCase(word)){
+        for (int i = 0; i < arrSize; i++) {
+            if (arr[i].equalsIgnoreCase(word)) {
                 count++;
             }
         }
@@ -15,13 +14,21 @@ public class lab4 {
         return count;
     }
 
-    public static void main (String[] args){
-        try (Scanner scnr = new Scanner(System.in)){
-            String[] wordList = new String [20];
+    public static void main(String[] args) {
+        try (Scanner scnr = new Scanner(System.in)) {
+            String[] wordList = new String[20];
             int wordListLength = scnr.nextInt();
 
-            
-        };
+            // build array
+            for (int i = 0; i < wordListLength; i++) {
+                wordList[i] = scnr.next();
+            }
+
+             for (int i = 0; i < wordListLength; i++) {
+                System.out.println(wordList[i] + " " + getWordFrequency(wordList, wordListLength, wordList[i]));
+            }
+        }
+        
 
     }
 }
