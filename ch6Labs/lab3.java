5744154e46a5286914d0d7d06222703cc1fedf03
cseibert2667 <cseibert2667@gmail.com>
2023-04-12 18:47:07 -0700

    lab3 done

--- ch6Labs/lab3.java
+++ ch6Labs/lab3.java
@@ -6,18 +6,36 @@ import java.util.Arrays;
 public class lab3 {
 
     public static void sortArray(int[] myArr, int arrSize){
-
+        for (int i = 0; i < arrSize; i++) {
+            for (int j = arrSize - 1; j > i; j--) {
+                if (myArr[j] > myArr[j-1]){
+                    int temp = myArr[j];
+                    myArr[j] = myArr[j-1];
+                    myArr[j-1] = temp;
+                }
+            }
+        }
     }
 
 
     public static void main (String[] args){
-        Scanner scnr = new Scanner(System.in);
-        int[] numbers = new int[20];
+        try (Scanner scnr = new Scanner(System.in)) {
+            int[] numbers = new int[20];
 
-        int arrLength = scnr.nextInt();
+            int arrLength = scnr.nextInt();
 
-        for (int i = 0; i < arrLength; i++) {
-            numbers[i] = scnr.nextInt();
+            // build array
+            for (int i = 0; i < arrLength; i++) {
+                numbers[i] = scnr.nextInt();
+            }
+
+            sortArray(numbers, arrLength);
+
+            for (int i = 0; i < arrLength; i++) {
+                System.out.print(numbers[i] + ",");
+            }
+
+            System.out.println();
         }
 
     }
