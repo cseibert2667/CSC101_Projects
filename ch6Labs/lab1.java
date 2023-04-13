d53281aea66d2c9f07a3de444b9fc5b3cb761970
cseibert2667 <cseibert2667@gmail.com>
2023-04-12 18:09:41 -0700

    lab1 done

--- ch6Labs/lab1.java
+++ ch6Labs/lab1.java
@@ -1,15 +1,46 @@
 package ch6Labs;
+
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class lab1 {
 
-    public static void main (String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        int array[] = new int[4];
+    public static void swapValues (int[] arr){
+        int temp;
 
-        for (int i = 0; i < array.length; i++) {
+        temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+        
+        temp = arr[2];
+        arr[2] = arr[3];
+        arr[3] = temp;
+    }
+
+    public static void main (String[] args) {
+        try (Scanner scnr = new Scanner(System.in)) {
+            int array[] = new int[4];
+
+            // build array
+            for (int i = 0; i < array.length; i++) {
+                array[i] = scnr.nextInt();
+            }
+
+            swapValues(array);
             
+            for (int i = 0; i < array.length; i++) {
+               
+               if (i == 3) {
+                  System.out.println(array[i]);
+               }
+               else {
+                System.out.print(array[i] + " ");
+               }
+            }
+            
+
+            // test print
+            System.out.println(Arrays.toString(array));
         }
 
     }
