2a3f8e5de6852f609f56fb022bebd25202b3d1e0
cseibert2667 <cseibert2667@gmail.com>
2023-04-12 18:50:04 -0700

    formatting

--- ch6Labs/lab3.java
+++ ch6Labs/lab3.java
@@ -1,24 +1,22 @@
 package ch6Labs;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class lab3 {
 
-    public static void sortArray(int[] myArr, int arrSize){
+    public static void sortArray(int[] myArr, int arrSize) {
         for (int i = 0; i < arrSize; i++) {
             for (int j = arrSize - 1; j > i; j--) {
-                if (myArr[j] > myArr[j-1]){
+                if (myArr[j] > myArr[j - 1]) {
                     int temp = myArr[j];
-                    myArr[j] = myArr[j-1];
-                    myArr[j-1] = temp;
+                    myArr[j] = myArr[j - 1];
+                    myArr[j - 1] = temp;
                 }
             }
         }
     }
 
-
-    public static void main (String[] args){
+    public static void main(String[] args) {
         try (Scanner scnr = new Scanner(System.in)) {
             int[] numbers = new int[20];
 
