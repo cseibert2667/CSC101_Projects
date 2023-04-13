2f5c35d97998c58f31a8b2ee978484a730452907
cseibert2667 <cseibert2667@gmail.com>
2023-04-12 18:29:13 -0700

    lab 2 done

--- ch6Labs/lab2.java
+++ ch6Labs/lab2.java
@@ -1,9 +1,30 @@
 package ch6Labs;
+
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.Random;
 
 public class lab2 {
-    public static void main (String[] args){
-        
+
+    public static String coinFlip(Random rand) {
+        int flip = rand.nextInt(2);
+
+
+        if (flip == 1) {
+            return "Heads";
+        } else {
+            return "Tails";
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scnr = new Scanner(System.in)) {
+            Random rand = new Random(2);
+            int flips = scnr.nextInt();
+
+            for (int i = 0; i < flips; i++) {
+                System.out.println(coinFlip(rand));
+            }
+        }
+
     }
 }
