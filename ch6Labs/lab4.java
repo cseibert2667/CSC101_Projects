1ee6d401f365d2d6e7e374ac5e8bb558212c7262
Cooper <cseibert2667@gmail.com>
2023-04-13 10:12:53 -0700

    lab4 start

---
+++ ch6Labs/lab4.java
@@ -0,0 +1,16 @@
+package ch6Labs;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class lab4 {
+    public static int getWordFrequency (String[] arr, int arrSize, String word) {
+
+
+        return 0;
+    }
+
+    public static void main (String[] args){
+
+    }
+}
