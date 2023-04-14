9e6312e75750eb04af96c68c1062b8d57cfa4ce6
Cooper <cseibert2667@gmail.com>
2023-04-14 10:22:38 -0700

    proj 3 python start

---
+++ project3.py
@@ -0,0 +1,10 @@
+jersey_num = [0] * 5
+rating = [0] * 5
+
+for i in range(5):
+    jersey_num[i] = input("Enter Player " + (str(i + 1)) + "'s jersey number: ")
+    rating[i] = input("Enter Player " + (str(i + 1)) + "'s rating: ")
+
+
+
+print(jersey_num)
