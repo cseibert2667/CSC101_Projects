375bff5f42c2c84a9c439e06eac42d9e6ff9f5ef
Cooper <cseibert2667@gmail.com>
2023-04-14 10:46:47 -0700

    menu print

--- project3.py
+++ project3.py
@@ -1,12 +1,29 @@
 jersey_num = [0] * 5
 rating = [0] * 5
 
+# GET INPUTS FOR PLAYER INFO
 for i in range(5):
     jersey_num[i] = input("Enter Player " + (str(i + 1)) + "'s jersey number: ")
     rating[i] = input("Enter Player " + (str(i + 1)) + "'s rating: ")
+print('\n')
 
+# PRINT ROSTER
 print("ROSTER")
 for i in range(5):
     # print("hi" + jersey_num[i])
     print("Player " + (str(i + 1)) + " -- Jersey number: " + (str(jersey_num[i])) + ", Rating: " + (str(rating[i])))
+print('\n')
 
+# PRINT MENU
+print(
+    "MENU\n" +
+    "u - Update player rating\n"
+    "a - Output players above a rating\n"
+    "r - Replace player\n"
+    "o - Output roster\n"
+    "q - Quit\n"
+    "Choose an option: "
+    menuOption = input("Choose an option: ")
+)
+
+# MENU CHOICES
