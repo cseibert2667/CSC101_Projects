52aa9ea017874fc293715b66049781a46a66c49a
Cooper <cseibert2667@gmail.com>
2023-04-14 10:30:26 -0700

    roster print

--- project3.py
+++ project3.py
@@ -5,6 +5,8 @@ for i in range(5):
     jersey_num[i] = input("Enter Player " + (str(i + 1)) + "'s jersey number: ")
     rating[i] = input("Enter Player " + (str(i + 1)) + "'s rating: ")
 
+print("ROSTER")
+for i in range(5):
+    # print("hi" + jersey_num[i])
+    print("Player " + (str(i + 1)) + " -- Jersey number: " + (str(jersey_num[i])) + ", Rating: " + (str(rating[i])))
 
-
-print(jersey_num)
