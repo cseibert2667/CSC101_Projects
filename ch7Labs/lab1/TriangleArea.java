7afc9d179851a90250fc2c5f2e56f9aaf19af839
cseibert2667 <cseibert2667@gmail.com>
2023-04-19 18:40:59 -0700

    cleanup of ch7 labs

--- ch7Labs/lab1/TriangleArea.java
+++ ch7Labs/lab1/TriangleArea.java
@@ -8,18 +8,14 @@ public class TriangleArea {
         Triangle triangle1 = new Triangle();
           Triangle triangle2 = new Triangle();
 
-          // TODO: Read and set base and height for triangle1 (use setBase() and setHeight())
           triangle1.setBase(scnr.nextDouble());
           triangle1.setHeight(scnr.nextDouble());
           
-          // TODO: Read and set base and height for triangle2 (use setBase() and setHeight())
           triangle2.setBase(scnr.nextDouble());
           triangle2.setHeight(scnr.nextDouble());
           
           System.out.println("Triangle with smaller area:");
           
-          // TODO: Determine smaller triangle (use getArea())
-          //       and output smaller triangle's info (use printInfo())
           if (triangle1.getArea() < triangle2.getArea()) {
              triangle1.printInfo();
           }
--- ch7Labs/lab2/Car.java
+++ ch7Labs/lab2/Car.java
@@ -1,8 +1,9 @@
 package ch7Labs.lab2;
 
+import java.lang.Math;
+
 public class Car {
     private int modelYear; 
-    // TODO: Declare purchasePrice field (int)
     private int purchasePrice;
  
     private int currentValue;
@@ -15,12 +16,10 @@ public class Car {
        return modelYear;
     }
     
-    // TODO: Define setPurchasePrice() method
     public void setPurchasePrice(int userPrice){
         purchasePrice = userPrice;
     }
     
-    // TODO: Define getPurchasePrice() method
     public int getPurchasePrice() {
         return purchasePrice;
     }
@@ -34,7 +33,6 @@ public class Car {
           Math.round(purchasePrice * Math.pow((1 - depreciationRate), carAge));
     }
     
-    // TODO: Define printInfo() method to output modelYear, purchasePrice, and currentValue
     public void printInfo(){
         System.out.println("Car's infortmation:");
         System.out.println("Model year: " + this.getModelYear());
