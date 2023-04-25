dab8013756f8d6c922ff43bb03ea20d8500c7888
Cooper <cseibert2667@gmail.com>
2023-04-25 10:20:27 -0700

    stash

--- test.java
+++ test.java
@@ -1,33 +1,12 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+
 public class test {
-    public static void updateScoreVals(int[] scoreVals, int updateVal) {
-        int i;
-  
-        for (i = 0; i < scoreVals.length; ++i) {
-            scoreVals[i] = scoreVals[i] * updateVal;
-        }
-     }
-     public static void printScoreVals(int[] scoreVals) {
-         int i;
-  
-         for (i = 0; i < scoreVals.length; ++i) {
-             System.out.println(scoreVals[i]);
-         }
-     }
-  
-     public static void main(String[] args) {
-        try (Scanner scnr = new Scanner(System.in)) {
-            final int NUM_SCORES = 3;
-            int[] quizScores = new int[NUM_SCORES];
-            int scoreUpdate = 2;
-            int i;
-  
-            for (i = 0; i < quizScores.length; ++i) {
-                quizScores[i] = scnr.nextInt();
-            }
-  
-            updateScoreVals(quizScores, scoreUpdate);
-            printScoreVals(quizScores);
-        }
-     }
+    public static int addGrade(int[] allGrades, int grade, int listSize) {
+        return grade;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
