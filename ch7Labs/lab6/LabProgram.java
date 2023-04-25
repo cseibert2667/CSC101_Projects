af8baa6d5bd70b43469387df1a9ca9946808b819
Cooper <cseibert2667@gmail.com>
2023-04-25 13:36:56 -0700

    done i guess

--- ch7Labs/lab6/LabProgram.java
+++ ch7Labs/lab6/LabProgram.java
@@ -3,11 +3,17 @@ package ch7Labs.lab6;
 import java.util.Scanner;
 
 public class LabProgram {
-   public static void main(String[] args) {
-      try (Scanner scnr = new Scanner(System.in)) {
+    public static void main(String[] args) {
+        try (Scanner scnr = new Scanner(System.in)) {
+            VendingMachine vend = new VendingMachine();
+            int amount = scnr.nextInt();
+
+            vend.purchase(amount);
+            amount = scnr.nextInt();
+            vend.restock(amount);
+            vend.report();
+            /* Type your code here. */
+
+        }
     }
-      
-      /* Type your code here. */ 
-      
-   }
 }
