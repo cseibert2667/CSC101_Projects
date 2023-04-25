30e4583c8b8d91177b133eb7b0847501c1276d50
Cooper <cseibert2667@gmail.com>
2023-04-25 12:38:00 -0700

    lab 6 7.25 starting code

---
+++ ch7Labs/lab6/LabProgram.java
@@ -0,0 +1,13 @@
+package ch7Labs.lab6;
+
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      try (Scanner scnr = new Scanner(System.in)) {
+    }
+      
+      /* Type your code here. */ 
+      
+   }
+}
