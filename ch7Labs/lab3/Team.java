5ee77cb0fe1deb13ae583d4dc04927bec441267c
Cooper <cseibert2667@gmail.com>
2023-04-25 10:33:55 -0700

    fields & mutator methods

--- ch7Labs/lab3/Team.java
+++ ch7Labs/lab3/Team.java
@@ -2,10 +2,24 @@ package ch7Labs.lab3;
 
 public class Team {
     // TODO: Declare private fields - name, wins, losses
+    private String name;
+    private int wins;
+    private int losses;
     
     
     // TODO: Define mutator methods - 
     //       setName(), setWins(), setLosses()
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWins(int wins){
+        this.wins = wins;
+    }
+
+    public void setLosses(int losses){
+        this.losses = losses;
+    }
     
     
     // TODO: Define accessor methods - 
