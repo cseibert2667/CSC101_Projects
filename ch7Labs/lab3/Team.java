b950d73917d5e6aec23676ae22d22e291c8caab4
Cooper <cseibert2667@gmail.com>
2023-04-25 10:47:55 -0700

    functioning as intended

--- ch7Labs/lab3/Team.java
+++ ch7Labs/lab3/Team.java
@@ -1,34 +1,49 @@
 package ch7Labs.lab3;
 
 public class Team {
-    // TODO: Declare private fields - name, wins, losses
+    // private fields
     private String name;
     private int wins;
     private int losses;
-    
-    
-    // TODO: Define mutator methods - 
-    //       setName(), setWins(), setLosses()
+
+    // mutator methods
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setWins(int wins){
+    public void setWins(int wins) {
         this.wins = wins;
     }
 
-    public void setLosses(int losses){
+    public void setLosses(int losses) {
         this.losses = losses;
     }
-    
-    
-    // TODO: Define accessor methods - 
-    //       getName(), getWins(), getLosses()
-    
-    
-    // TODO: Define getWinPercentage()
-    
-    
-    // TODO: Define printStanding()
-    
- }
+
+    // accessor methods
+    public String getName() {
+        return name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public double getWinPercentage(double wins, double losses) {
+        return wins / (wins + losses);
+    }
+
+    // print method
+    public void printStanding() {
+        System.out.printf("Win percentage: %.2f \n", getWinPercentage(wins, losses));
+        if (getWinPercentage(wins, losses) >= 0.5) {
+            System.out.println("Congratulations, Team " + name + " has a winning average!");
+        } else {
+            System.out.println(name + " has a losing average.");
+        }
+    }
+
+}
